aef76b0b6fdcea7807dbd9a5def0d118d62ae688
SachiWelagedara <info.sachinihansika@gmail.com>
2026-01-11 14:45:10 +0530

    Updated user authentication with hashed passwords

--- models/customer.model.js
+++ models/customer.model.js
@@ -1,14 +1,31 @@
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
 const Customer = {
     register: (data, callback) => {
-        const sql = "INSERT INTO customers (name, email, phone, password) VALUES (?, ?, ?, ?)";
-        db.query(sql, [data.name, data.email, data.phone, data.password], callback);
+        bcrypt.hash(data.password, 10, (err, hashedPassword) => {
+            if (err) return callback(err);
+
+            const sql = "INSERT INTO customers (name, email, phone, password) VALUES (?, ?, ?, ?)";
+            db.query(sql, [data.name, data.email, data.phone, hashedPassword], callback);
+        });
     },
 
     login: (data, callback) => {
-        const sql = "SELECT id, name, email, phone FROM customers WHERE email = ? AND password = ?";
-        db.query(sql, [data.email, data.password], callback);
+        const sql = "SELECT id, name, email, phone, password FROM customers WHERE email = ?";
+        db.query(sql, [data.email], (err, results) => {
+            if (err) return callback(err);
+            if (results.length === 0) return callback(null, []);
+
+            const user = results[0];
+            bcrypt.compare(data.password, user.password, (err, isMatch) => {
+                if (err) return callback(err);
+                if (!isMatch) return callback(null, []);
+
+                delete user.password;
+                callback(null, [user]);
+            });
+        });
     }
 };
 
--- models/user.model.js
+++ models/user.model.js
@@ -1,14 +1,31 @@
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
 const User = {
     register: (data, callback) => {
-        const sql = "INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)";
-        db.query(sql, [data.username, data.email, data.password, data.role], callback);
+        bcrypt.hash(data.password, 10, (err, hashedPassword) => {
+            if (err) return callback(err);
+
+            const sql = "INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)";
+            db.query(sql, [data.username, data.email, hashedPassword, data.role], callback);
+        });
     },
 
     login: (data, callback) => {
-        const sql = "SELECT * FROM users WHERE email = ? AND password = ?";
-        db.query(sql, [data.email, data.password], callback);
+        const sql = "SELECT * FROM users WHERE email = ?";
+        db.query(sql, [data.email], (err, results) => {
+            if (err) return callback(err);
+            if (results.length === 0) return callback(null, []);
+
+            const user = results[0];
+            bcrypt.compare(data.password, user.password, (err, isMatch) => {
+                if (err) return callback(err);
+                if (!isMatch) return callback(null, []);
+
+                delete user.password;
+                callback(null, [user]);
+            });
+        });
     }
 };
 
